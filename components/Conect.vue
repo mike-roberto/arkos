eb40635fc9e9cf4b71bce9a083801fab85c1e44a
Mike <maicon.ohm@gmail.com>
2024-02-27 17:53:49 -0300

    vamo

---
+++ components/Conect.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-layout>
+ <v-container class="mt-10">
+    <v-row>
+        <v-col cols="12">
+            <v-card
+          class="mx-auto"
+          max-width="auto"
+          :variant="variant"
+          flat
+        >
+          <v-card-item>
+            <div>
+              <div class="text-overline mb-1">
+            Soluções especializadas
+              </div>
+              <div class="text-h3 mb-1 font-weight-bold">
+                Conheça o ecossistema arkos
+              </div>
+              <div class=" text-h6 mt-5">Ambiente com soluções modulares para a digitalização <br> ”end-to-end” das operações no Mercado de Capitais.
+
+</div>
+            </div>
+          </v-card-item>
+
+          <v-card-actions>
+            <v-chip-group size="lg" class="mt-10">
+  <v-chip size="x-large" density="comfortable">Arquitetos</v-chip>
+  <v-chip size="x-large" density="comfortable">Engenheiros</v-chip>
+  <v-chip size="x-large" density="comfortable">Técnicos</v-chip>
+  <v-chip size="x-large" density="comfortable">Empresas</v-chip>
+  <v-chip size="x-large" density="comfortable">Construtoras</v-chip>
+  <v-chip size="x-large" density="comfortable">Fornecedores</v-chip>
+  <v-chip size="x-large" density="comfortable">Consultores</v-chip>
+  <v-chip size="x-large" density="comfortable">Projetistas</v-chip>
+  <v-chip size="x-large" density="comfortable">Tecnólogos</v-chip>
+</v-chip-group>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+    </v-row>
+ </v-container>
+
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Origin.vue
@@ -0,0 +1,144 @@
+<template>
+    <v-container class="mt-5">
+<v-card flat class="">
+    <v-tabs
+      class=""
+      v-model="tab"
+      align-tabs="start"
+      bg-color="white"
+      dark    >
+      <v-tab value="one">Tecnologia</v-tab>
+      <v-tab value="two">Engenharia</v-tab>
+      <v-tab value="three">Infraestrutura</v-tab>
+      <v-tab value="four">Arquitetura & Construção</v-tab>
+    </v-tabs>
+
+    <v-card-text>
+      <v-window v-model="tab">
+        <v-window-item value="one">
+        <v-row class="mt-10">
+          
+          <v-col cols="12" md="4" class="align-center align-justify">
+            <v-card flat height="600px">
+         <v-img class="mt-15" size = '600px' src="https://img.freepik.com/free-photo/cloud-storage-banner-background_53876-108506.jpg?w=996&t=st=1708383423~exp=1708384023~hmac=e24ba3466fba254b19c2b7286c9aa0d29e39132bd637d3d20a31d6e0129fdca5"/>
+        </v-card>
+      </v-col>
+  
+      <v-col cols="12" md="4">
+        <v-card
+        class="mx-auto"
+        max-width="344"
+        title="BIM"
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+       
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        title="PowerBI"
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+       
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        title=""
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+        
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        title="Avatars"
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+        
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card
+        class="mx-auto"
+        max-width="344"
+        title="Avatars"
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+       
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        title="Avatars"
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+       
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        title="Avatars"
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+        
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        title="Avatars"
+        flat
+        prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+        
+      >
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+      </v-card> 
+      </v-col>
+    </v-row>
+        </v-window-item>
+
+        <v-window-item value="two">
+            <v-card flat height="600px">
+         <v-img  size = '600' src="/telas.jpg"/>
+        </v-card>
+        </v-window-item>
+
+        <v-window-item value="three">
+            <v-card flat height="600px">
+         <v-img  size = '600' src="/telas.jpg"/>
+        </v-card>
+        </v-window-item>
+
+        <v-window-item value="four">
+            <v-card height="600px">
+         <v-img  size = '600' src="/telas.jpg"/>
+        </v-card>
+        </v-window-item>
+      </v-window>
+      
+    </v-card-text>
+  </v-card>
+</v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      tab: null,
+    }),
+  }
+</script>
